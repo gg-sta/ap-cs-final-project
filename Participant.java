f9bb5918310f264c544dca539ba446e408525823
Mint Admin <mint-admin@linuxmint.tail79210f.ts.net>
2026-06-02 13:01:48 -0700

    added some info in java file

---
+++ Participant.java
@@ -0,0 +1,24 @@
+public class Participant {
+    /*
+    required fields:
+    
+    - getters/setters
+        - name
+        - grade
+        - studentID
+        - activities
+        - score
+
+        - constructor
+
+    - toString()
+        - Example: Participant p = new Participant("Ava", 11, 2045, {“Robotics”, “Basketball”, “Debate”}, 95);
+    */
+
+    private String name;
+    private int grade;
+    private int studentID;
+    private string activities;
+    private int score;
+    
+}
